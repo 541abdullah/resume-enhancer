5829bd3c16d73fbcc2c01eefa62d3c3679b2651d
abdullah541 <abdullahawais51020@gmail.com>
2026-01-02 01:37:54 +0300

    seven commit

--- utils/pdf_converter.py
+++ utils/pdf_converter.py
@@ -1,18 +1,18 @@
-import subprocess
-import os
+# import subprocess
+# import os
 
-def docx_to_pdf(docx_path: str):
-    output_dir = os.path.dirname(docx_path) or "."
+# def docx_to_pdf(docx_path: str):
+#     output_dir = os.path.dirname(docx_path) or "."
 
-    subprocess.run(
-        [
-            "libreoffice",
-            "--headless",
-            "--convert-to",
-            "pdf",
-            docx_path,
-            "--outdir",
-            output_dir,
-        ],
-        check=True,
-    )
+#     subprocess.run(
+#         [
+#             "libreoffice",
+#             "--headless",
+#             "--convert-to",
+#             "pdf",
+#             docx_path,
+#             "--outdir",
+#             output_dir,
+#         ],
+#         check=True,
+#     )
